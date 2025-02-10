625e57b17b55cc5b0c1f3acfa8c483663d261ab2
crackbob <crackbob.999@gmail.com>
2025-02-10 01:22:29 -0500

    block picker

--- src/module/modules/misc/SpoofBlock.js
+++ src/module/modules/misc/SpoofBlock.js
@@ -4,15 +4,52 @@ import stores from "../../../utils/stores";
 
 export default class SpoofBlock extends Module {
     constructor() {
-        super("SpoofBlock", "Misc", {
-            "Block ID": 1
-        });
-        
+        super("SpoofBlock", "Misc");
+        this.blockID = 0;
     }
 
-    onEnable() {
-        let itemID = this.options["Block ID"];
-        stores.itemMgrStore[1][stores.itemMgrStore.selectedItem][0] = itemID;
-        hooks.gameWorld.player.currentHandItemId = itemID;
+    onRender () {
+        stores.itemMgrStore[1][stores.itemMgrStore.selectedItem][0] = this.blockID;
+        hooks.gameWorld.player.currentHandItemId = this.blockID;
+    }
+
+    onEnable () {
+        let menuContainer = document.createElement("div");
+        menuContainer.className = "blockSelector";
+        document.body.appendChild(menuContainer);
+
+        let itemContainer = document.createElement("div");
+        Object.assign(itemContainer.style, {
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "center"
+        });
+        menuContainer.appendChild(itemContainer);
+
+        function addButton(id) {
+            let button = document.createElement("button");
+            Object.assign(button.style, {
+                border: "none",
+                background: "none",
+                margin: "10px"
+            });
+            let img = document.createElement("img");
+            Object.assign(img.style, {
+                width: "40px",
+                height: "40px",
+                objectFit: "cover"
+            });
+            img.src = $assetsUrls["defaultSurvival/renderItems/" + id + ".png"];
+            button.appendChild(img);
+            button.addEventListener("click", () => {
+                stores.itemMgrStore[1][stores.itemMgrStore.selectedItem][0] = id;
+                hooks.gameWorld.player.currentHandItemId = id;
+                this.blockID = id;
+                menuContainer.remove();
+            });
+            itemContainer.appendChild(button);
+        }
+
+        Object.keys(hooks.gameWorld.allItems).forEach(key => addButton(key));
     }
 }
