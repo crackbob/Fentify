28ee7c60fc4c4f48f2446381666556326ee91f0c
crackbob <crackbob.999@gmail.com>
2025-07-12 22:39:44 -0400

    fix

--- src/hooks.js
+++ src/hooks.js
@@ -1,13 +1,28 @@
-import events from "./events";
-
 export default {
-    init: async function () {
-        let safeImport = (src) => eval(`(async () => { return await import("${src}")})()`);
-        let mainModule = await safeImport(Object.values(document.scripts).find(script => script?.src?.includes("index")).src);
-        let roomManagerModule = await safeImport(Object.values(document.getElementsByTagName("link")).find(link => link?.href?.includes("room")).href);
+    get mainIndexUrl () {
+        return Object.values(document.scripts).find(script => script?.src?.includes("index")).src
+    },
+    
+    async getAllChunks () {
+        let code = await fetch(this.mainIndexUrl)
+        .then(res => res.text())
+        .then(code => code.split('\n')[0]);
 
-        this.stores = [...Object.values(mainModule), ...Object.values(roomManagerModule)]
-            .filter(exports => exports?.$id)
-            .reduce((acc, exports) => (acc[exports.$id] = exports(), acc), {});
+        let __vite__mapDeps = Function(code + `return __vite__mapDeps`)()
+        __vite__mapDeps([1]);
+        return __vite__mapDeps.f.filter(chunkUrl => chunkUrl.includes("js"));
+    },
+
+    safeImport (src) {
+        return eval(`(async () => { return await import("${src}")})()`);
+    },
+
+    init: async function () {
+        let allChunks = (await this.getAllChunks()).map(url => "https://" + location.host + "/" + url);
+        allChunks.push(this.mainIndexUrl);
+        allChunks = allChunks.filter(url => !url.includes("General")); // causes errors idk wtf
+        let importedModules = await Promise.all(allChunks.map(url => this.safeImport(url)));
+        let allModuleExports = importedModules.flatMap(module => Object.values(module));
+        this.stores = Object.values(allModuleExports).filter(exports => exports?.$id).reduce((acc, exports) => (acc[exports.$id] = exports(), acc), {});
     }
 }
